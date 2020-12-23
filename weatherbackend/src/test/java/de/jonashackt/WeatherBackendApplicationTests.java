dc4635ee075013211a7f3c262b6d8367b482dc1a
Jonas Hecht <jonas.hecht@codecentric.de>
2020-12-23 10:37:19 +0100

    Migrating most services to JUnit 5 (which is now Spring Boot 2.4.x+ default). Leaving the docker-network-client to JUnit 4, since testcontainers DockerCompose module is based on it.

--- weatherbackend/src/test/java/de/jonashackt/WeatherBackendApplicationTests.java
+++ weatherbackend/src/test/java/de/jonashackt/WeatherBackendApplicationTests.java
@@ -8,20 +8,17 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(
         classes = WeatherBackendApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
